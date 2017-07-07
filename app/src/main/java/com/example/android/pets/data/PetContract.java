3388fb67f69919a2362fc7d7f0d46934d3b61215
damwilin <damian.wilinski@gmail.com>
2017-07-07 10:48:34 +0200

    Added PetHelper and implemented SQLiteDatabaseHelper

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -15,14 +15,21 @@ public final class PetContract {
     public static class PetEntry implements BaseColumns {
         public static final String TABLE_NAME = "pets";
 
-        public static final String COLUMN_NAME_ID = BaseColumns._ID;
-        public static final String COLUMN_NAME_NAME = "name";
-        public static final String COLUMN_NAME_BREED = "breed";
-        public static final String COLUMN_NAME_GENDER = "gender";
-        public static final String COLUMN_NAME_WEIGHT = "weight";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
 
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
         public static final int GENDER_UNKNOWN = 0;
+
+        public static final String TEXT_TYPE = " TEXT";
+        public static final String INTEGER_TYPE = " INTEGER";
+        public static final String COMMA_SEP = ",";
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,46 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.example.android.pets.data.PetContract.PetEntry.COLUMN_PET_BREED;
+import static com.example.android.pets.data.PetContract.PetEntry.COLUMN_PET_GENDER;
+import static com.example.android.pets.data.PetContract.PetEntry.COLUMN_PET_NAME;
+import static com.example.android.pets.data.PetContract.PetEntry.COLUMN_PET_WEIGHT;
+import static com.example.android.pets.data.PetContract.PetEntry.COMMA_SEP;
+import static com.example.android.pets.data.PetContract.PetEntry.INTEGER_TYPE;
+import static com.example.android.pets.data.PetContract.PetEntry.TABLE_NAME;
+import static com.example.android.pets.data.PetContract.PetEntry.TEXT_TYPE;
+import static com.example.android.pets.data.PetContract.PetEntry._ID;
+
+/**
+ * Created by Damian on 7/7/2017.
+ */
+
+public class PetDbHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Pets.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_ENTRIES = "CREATE TABLE " + TABLE_NAME + "(" +
+                _ID + " INTEGER PRIMARY KEY," +
+                COLUMN_PET_NAME + TEXT_TYPE + COMMA_SEP +
+                COLUMN_PET_BREED + TEXT_TYPE + COMMA_SEP +
+                COLUMN_PET_GENDER + INTEGER_TYPE + COMMA_SEP +
+                COLUMN_PET_WEIGHT + INTEGER_TYPE + ")";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
